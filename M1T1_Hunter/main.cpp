dcbd9da79dec8e2d2cf015dc9aa058cafd4c33dc
hunterm8745 <hunterm8745@student.faytechcc.edu>
2019-01-15 14:19:46 -0500

    First upload

---
+++ M1T1_Hunter/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// CSC 134
+// M1T1
+// Hunterm
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello from CSC-134!" << endl;
+    cout << "Quack" << endl;
+    return 0;
+}
